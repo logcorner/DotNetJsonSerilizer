027f093682217ccef59741ed4a04e7d65dd08d1b
Gora LEYE <tocane.technologies@gmail.com>
2022-05-19 13:17:45 +0200

    check arguments

--- ConsoleApp1/JsonDotNetProvider.cs
+++ ConsoleApp1/JsonDotNetProvider.cs
@@ -6,15 +6,32 @@ namespace ConsoleApp
     {
         public TEvent DeserializeObject<TEvent>(string serializedEvent, string eventType)
         {
+            if (string.IsNullOrWhiteSpace(serializedEvent))
+            {
+                throw new ArgumentNullException(nameof(serializedEvent));
+            }
+            if (string.IsNullOrWhiteSpace(eventType))
+            {
+                throw new ArgumentNullException(nameof(eventType));
+            }
             JsonSerializerOptions settings = new JsonSerializerOptions
             {
                 IncludeFields = true
             };
-            return (TEvent)JsonSerializer.Deserialize(serializedEvent, Type.GetType(eventType), settings);
+           var result = JsonSerializer.Deserialize(serializedEvent, Type.GetType(eventType) ?? throw new InvalidOperationException(), settings);
+           if (result == null)
+           {
+               throw new Exception($"cannot deserialize serializedEvent {serializedEvent}");
+           }
+           return (TEvent) result;
         }
 
         public string SerializeObject<TEvent>(TEvent domainEvent)
         {
+            if (domainEvent == null)
+            {
+                throw new ArgumentNullException(nameof(domainEvent));
+            }
             string serializedEvent = JsonSerializer.Serialize(domainEvent, domainEvent.GetType());
 
             return serializedEvent;
